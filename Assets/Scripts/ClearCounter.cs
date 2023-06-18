6f8fca1ae8b9d86285c65a56ee07a048a030356c
Vaibhav Kubre <v@kubre.in>
2023-06-18 11:39:39 +0530

    Added kitchen items scriptable objects

--- Assets/Scripts/ClearCounter.cs
+++ Assets/Scripts/ClearCounter.cs
@@ -4,12 +4,51 @@ using UnityEngine;
 
 public class ClearCounter : MonoBehaviour {
 
-    [SerializeField] private Transform tomatoPerfab;
+    [SerializeField] private KitchenObjectSO kitchenObjectSO;
+
     [SerializeField] private Transform counterTopPoint;
+    [SerializeField] private ClearCounter secondClearCounter;
+
+    private KitchenObject kitchenObject;
+
+    public bool testing;
+
+
+    private void Update() {
+        if (testing && Input.GetKeyDown(KeyCode.T)) {
+            if (kitchenObject != null) {
+                kitchenObject.SetClearCounter(secondClearCounter);
+            }
+        }
+    }
 
     public void Interact() {
-        
-        Transform tomatoTransform = Instantiate(tomatoPerfab, counterTopPoint);
-        tomatoTransform.localPosition = Vector3.zero;
+
+        if (kitchenObject == null) {
+            Transform kitchenObjectTransform = Instantiate(kitchenObjectSO.prefab, counterTopPoint);
+            kitchenObjectTransform.GetComponent<KitchenObject>().SetClearCounter(this);
+        } else {
+            Debug.Log(kitchenObject.GetClearCounter());
+        }
+    }
+
+    public Transform GetKitchenObjectFollowTransform() {
+        return counterTopPoint;
+    }
+
+    public void SetKitchenObject(KitchenObject kitchenObject) {
+        this.kitchenObject = kitchenObject;
+    }
+
+    public KitchenObject GetKitchenObject() {
+        return kitchenObject;
+    }
+
+    public void ClearKitchenObject() {
+        kitchenObject = null;
+    }
+
+    public bool HasKitchenObject() {
+        return kitchenObject != null;
     }
 }
